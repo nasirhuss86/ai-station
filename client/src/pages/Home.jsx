812962840d01d42ad838e4d599b76c9de525e379
Nasir Hussain <nasirhuss86@gmail.com>
2025-09-10 12:42:58 +0530

    hero section and more components done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 
 const Home = () =>{
     return (
        <>
        <Navbar/>
+       <Hero/>
+       <AiTools/>
 
        </>
     )
