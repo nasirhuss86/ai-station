42e764e138feae5e622493f6d82b5ec12dd409ba
Nasir Hussain <nasirhuss86@gmail.com>
2025-09-30 23:48:34 +0530

    dashboard button in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,52 @@
 import React from "react";
-import {assets} from "../assets/assets";
-import { useNavigate } from "react-router-dom"
-import { ArrowRight, User } from "lucide-react"
-import {useClerk , UserButton , useUser} from "@clerk/clerk-react"
-
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { ArrowRight, LayoutDashboard, LayoutDashboardIcon } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
-     const navigate = useNavigate();
-     const {user} = useUser();
-     const {openSignIn, openSignUp, signOut} = useClerk();
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
+  return (
+    <div className="fixed z-10 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+      
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-32 sm:w-44"
+        onClick={() => navigate("/")}
+      />
 
-     console.log(user);
-    return (
-       <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
-        <img src={assets.logo} alt="logo" className="w-32  sm:w-44" onClick={() => navigate('/')}/>
+     
+      <div className="flex items-center gap-4">
+        {user ? (
+          <>
+            
+            <button
+              onClick={() => navigate("/ai")}
+              className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-gray-300 px-3 py-2 hover:scale-105 active:scale-95 transition"
+            >
+              <LayoutDashboard className="w-4 h-4" />
+              Dashboard
+            </button>
 
-        {user ? <UserButton/>
-        :
-           ( <button onClick={openSignIn} className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"> Get Started <ArrowRight className="w-4 h-4"/></button>)
-        }
+            
+            <UserButton />
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+          >
+            Get Started
+            <ArrowRight className="w-4 h-4" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-} 
-export default Navbar
+export default Navbar;
